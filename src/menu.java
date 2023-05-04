e5908eb922a8d082d19ba2666dc52eda3e3e749f
MASSIMILIANO FEDRIZZI <max.fedrizzi@buonarroti.tn.it>
2023-05-04 08:45:34 +0200

    create interfacce prima finestra e menu

--- src/menu.java
+++ src/menu.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+
 public class menu {
 
+    private JButton aggiungereGiocatoreButton;
+    private JButton iniziarePartitaButton;
+    private JButton visualizzareClassificaButton;
 }
---
+++ src/primaFinestra.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class primaFinestra {
+    private JPanel primaFinestra;
+    private JButton AVANTIButton;
+}
