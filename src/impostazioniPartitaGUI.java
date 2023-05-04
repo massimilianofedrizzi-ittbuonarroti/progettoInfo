4e5aa241a5e752778fbe7c52ab2fbd2638c8ad51
MASSIMILIANO FEDRIZZI <max.fedrizzi@buonarroti.tn.it>
2023-05-04 10:30:19 +0200

    fine prima lezione

---
+++ src/impostazioniPartitaGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class impostazioniPartitaGUI {
+    private JLabel lblTitle;
+    private JLabel lblProfili;
+    private JList listProfili;
+    private JLabel lblDIfficoltà;
+    private JSlider slider1;
+}
