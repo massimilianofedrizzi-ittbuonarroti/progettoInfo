16224a92b617e5b09182547ae5b8bf40124f663b
MASSIMILIANO FEDRIZZI <max.fedrizzi@buonarroti.tn.it>
2023-05-11 08:51:00 +0200

    fix: sistemato la classe impostazionepartita e classifica

--- src/impostazioniPartitaGUI.java
+++ src/impostazioniPartitaGUI.java
@@ -1,9 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class impostazioniPartitaGUI {
+public class impostazioniPartitaGUI extends JFrame{
     private JLabel lblTitle;
     private JLabel lblProfili;
     private JList listProfili;
     private JLabel lblDIfficoltà;
     private JSlider slider1;
+    private JPanel settings;
+    private JButton btnBack;
+    private JButton btnAvanti;
+
+    public impostazioniPartitaGUI(){
+        setContentPane(settings);
+        setTitle("menu");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btnBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                menuGUI g2 = new menuGUI();
+            }
+        });
+        btnAvanti.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                partitaGUI p1 = new partitaGUI();
+            }
+        });
+    }
 }
