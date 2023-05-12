88436b308223057a1f7a54ea86f8dd53e9df8cdb
MASSIMILIANO FEDRIZZI <max.fedrizzi@buonarroti.tn.it>
2023-05-12 14:51:59 +0200

    fix: fixato generazione matrice con bombe casuali(max 4)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
-       /* boolean sotto[][]=new boolean[4][4];
+        boolean sotto[][]=new boolean[4][4];
         for(int i = 0; i< 2; i++){
-            int j = rand.nextInt()%4;
-            int v = rand.nextInt()%4;
+            int j = rand.nextInt(4);
+            int v = rand.nextInt(4);
             sotto[j][v] = true;
-        }*/
-        primaFinestraGUI finestra = new primaFinestraGUI(null);
+        }
+        primaFinestraGUI finestra = new primaFinestraGUI(sotto);
 
     }
 }
